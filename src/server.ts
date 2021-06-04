d30a848ce2be8e03c35930087ed0e74392639708
donald_h <donald_h@USMB128986.local>
2021-06-04 11:20:41 -0700

    added client side scripting

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
-
+//Dependencies
 import express, { Application, Request, Response, NextFunction } from 'express';
+import path from 'path';
+const app: Application = express();
 
-const func = require('./simple/func')
-const app: Application = express()
-const PORT = process.env.PORT || 3000
+const func = require('./simple/func');
 
+
+//Middleware
+//Static public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Routes
 app.get('/', (req: Request, res: Response, next: NextFunction) => res.send("number is: " + func(2, 3)));
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`running on port ${PORT}`);
-})
+});
