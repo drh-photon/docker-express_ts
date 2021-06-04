9a3455d6c15e16db1d66d47b5f8d7027329c29e6
donald_h <donald_h@USMB128986.local>
2021-06-04 10:37:32 -0700

    modified Dockerfile to map to localhost port -- docker functional

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 
-import express, {Application, Request, Response, NextFunction }from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
+
 const func = require('./simple/func')
 const app: Application = express()
-const port = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
-app.get('/', (req:Request, res: Response, next: NextFunction) => res.send("number is: " + func(1,2)));
+app.get('/', (req: Request, res: Response, next: NextFunction) => res.send("number is: " + func(2, 3)));
 
-app.listen(port, () => {
-console.log(`running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`running on port ${PORT}`);
 })
